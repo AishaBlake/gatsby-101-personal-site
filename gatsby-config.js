83d4c7910c466d5eef510d11e49726300b60aaa9
Aisha Blake <aisha.g.blake@gmail.com>
2020-07-24 19:11:40 -0400

    Add siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,21 @@
 module.exports = {
+    siteMetadata: {
+        title: `Aisha Blake`,
+        author: `Aisha Blake`,
+        description: `Aisha Blake's personal site for Gatsby 101 at CodeLand`,
+        siteUrl: `https://gatsby-101-personal-site.netlify.app`,
+        alternateSite: `https://aisha.codes`,
+        social: [
+            {
+                name: `Twitter`,
+                url: `https://twitter.com/AishaBlake`,
+            },
+            {
+                name: `GitHub`,
+                url: `https://github.com/AishaBlake`,
+            },
+        ],
+    },
     plugins: [
         `gatsby-plugin-mdx`,
         {
