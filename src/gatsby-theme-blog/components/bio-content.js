6394831725a4fdb465a1a17dca412c64b509510e
Aisha Blake <aisha.g.blake@gmail.com>
2020-07-24 18:48:48 -0400

    Shadow bio content

---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react"
+
+/**
+ * Shadow me to add your own bio content
+ */
+
+const BioContent = () => (
+  <Fragment>
+    Words by <a href="https://aisha.codes/">Aisha Blake</a>.
+    <br />
+    I'm a speaker, teacher, and software engineer! I can't spell.
+  </Fragment>
+)
+
+export default BioContent
